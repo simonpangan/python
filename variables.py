8f15d99eacbbeb6c182fad39c755581c55512304
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-11-03 23:43:11 +0800

    feat: declaring variables

--- variables.py
+++ variables.py
@@ -13,3 +13,32 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+#No semi colon in  python and is dynamically type and weakly typed.
+
+# x = 1 #int
+# y = 2.5 #float
+# name = 'John' #str
+# is_cool = True #bool
+
+#Multiple assignment
+
+x, y, name, is_cool = (1, 2.5, 'John', True)
+
+print(x)
+
+# Basic Math
+
+a = x + y
+
+print(a)
+print(type(x))
+
+# Casting
+
+x = str(x)
+y = int(y)
+z = float(y)
+
+print(type(x), y)
+print(z)
