96b4047d361c486651d4451a06595fb844690e9a
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-11-04 20:19:01 +0800

    feat: classes in python

---
+++ classes.py
@@ -0,0 +1,3 @@
+# A class is like a blueprint for creating objects. An object has properties and methods(functions) associated with it. Almost everything in Python is an object
+# Python can be used using different programming paradigms such as procedural, object-oriented, and functional programming. Python supports object-oriented programming (OOP).
+
