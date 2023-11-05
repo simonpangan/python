df310442ef1adb91e0842e319faeafc74270d08a
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-11-05 22:20:21 +0800

    feat: encapsulation

--- oop.py
+++ oop.py
@@ -2,8 +2,63 @@
 # IF WE DON'T NEED IT DON'T DO IT
 # This is why in python our classes are sometimes very thin. Example is encaptulation
 # In python we have the option to do it or not do it. The person using your classses
-# you can trust them that they are well educated 
+# you can trust them that they are well educated and are going to use your classes in
+# an appropriate way. WHere as in other language such as java, you have to restrict it
 
 #Encapsulation
-#In python we don't encapsulation 
-#
+# In python encapsulation is more like an optional thing. We can use it or not use it. 
+# Unlike in java, c# and other languages where it is a must.
+# We don't really need to create those unless we need to. 
+# That how python and its developers approach it and is considered as a best practice
+
+class Customer:
+	def __init__(self, name, membership_type):
+		self.name = name
+		self.membership_type = membership_type
+	
+	# _ prefix is a convention that this is a private variable
+	@property
+	def name(self):
+		#do something here
+		return self._name
+
+	@name.setter
+	def name(self, name):
+		#do something here
+		self._name = name
+	
+	@name.deleter
+	def name(self):
+		del self._name
+
+	def update_membership(self, new_membership):
+		self.membership_type = new_membership
+  
+	def read_customer():
+		print('Hi Customer')
+  
+	def print_all_customers(customers):
+		for customer in customers: 
+			print(customer)
+	
+	def __str__(self): 
+		return self.name + " " + self.membership_type
+
+	def __eq__(self, other): 
+		if (self.name == other.name and 
+			self.membership_type == other.membership_type
+      	): 
+			return True
+
+		return False
+
+	__repr__ = __str__
+
+customers = [
+	Customer("Simon Pangan", "Gold"),
+	Customer("Caleb Curry", "Gold"),
+]
+
+print(customers[1].name)
+# del customers[1].name
+print(customers)
