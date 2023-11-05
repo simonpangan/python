b66c92ee5ecea83c21381850748ba820890dde49
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-11-05 20:38:32 +0800

    feat: OOP in python

---
+++ oop.py
@@ -0,0 +1,9 @@
+# In python, unlike java and any other language it is more free. Our approach is 
+# IF WE DON'T NEED IT DON'T DO IT
+# This is why in python our classes are sometimes very thin. Example is encaptulation
+# In python we have the option to do it or not do it. The person using your classses
+# you can trust them that they are well educated 
+
+#Encapsulation
+#In python we don't encapsulation 
+#
